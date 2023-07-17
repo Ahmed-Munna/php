90a9d60b7c1e355ed0af24e940b84ee6876dca9c
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-07-18 03:03:35 +0600

    oop php

--- somethingNew/oop/namespace/Main.php
+++ somethingNew/oop/namespace/Main.php
@@ -5,9 +5,14 @@ use \Instument\String\Guitter as Gui;
 use \Instument\String\Mendalin as Men;
 use \Instument\Keyboard\Piano as pi;
 
-include('Guitter.php');
-include('Mendalin.php');
-include('Piano.php');
+spl_autoload_register(function($file) {
+    $slug = str_replace("Instument\\", "", $file) . ".php";
+    $slug = str_replace("\\" , "/" , $slug);
+    include($slug);
+});
+// include('Guitter.php');
+// include('Mendalin.php');
+// include('Piano.php');
 
 Gui::guitter();
 Men::mendalin();
