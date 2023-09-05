0b1c612b5091ca08d553fcd76ed0054b03a8167e
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-09-06 04:20:14 +0600

    js

---
+++ __new/types/type.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// $i = 214748364748222222222222222;
+// $s = "asdfas";
+
+// $x = 8 - 6.4;  // which is equal to 1.6
+// $y = 1.6;
+
+// echo var_dump($x == $y);
+
+
+$srt = "Something";
+
+echo $srt[0];
+
+
+
+
+
+
