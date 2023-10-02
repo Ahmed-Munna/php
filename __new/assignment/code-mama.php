2fb9a2737d00f1e6787256184cf6a3fba9a2c3cd
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-10-02 16:18:17 +0600

    php

---
+++ __new/assignment/code-mama.php
@@ -0,0 +1,16 @@
+<?php
+
+// $line = trim(fgets(STDIN));
+
+// $arr = str_split($name);
+// $uniqueArr = array_unique($arr);
+// $convartStr = join('', $uniqueArr);
+
+// echo $convartStr;
+
+$A = trim(fgets(STDIN));
+$B = "hello";
+$c = strrev($B);
+echo $c;
+
+// print(($A == strrev($B))? 'YES' : 'NO');
